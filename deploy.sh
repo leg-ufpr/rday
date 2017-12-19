62d3c1c85339a4e97b854025b9a3313c32c86af8
Fernando Mayer <fernandomayer@gmail.com>
2017-12-18 22:59:06 -0200

    Muda deploy para Rscript

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ USER=rday
 HOST=leg.ufpr.br
 DIR=/home/rday/
 
-hugo && rsync -avz docs/ ${USER}@${HOST}:${DIR}
+Rscript -e 'rmarkdown::render_site()' && rsync -avz --delete docs/ ${USER}@${HOST}:~/${DIR}
 
 exit 0
