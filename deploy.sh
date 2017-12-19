39b8c42d762f687df4524365c26db7af8b6ad658
Fernando Mayer <fernandomayer@gmail.com>
2017-12-18 23:15:10 -0200

    Arruma deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ USER=rday
 HOST=leg.ufpr.br
 DIR=/home/rday/
 
-Rscript -e 'rmarkdown::render_site()' && rsync -avz --delete docs/ ${USER}@${HOST}:~/${DIR}
+Rscript -e 'rmarkdown::render_site()' && rsync -avz --delete docs/ ${USER}@${HOST}:${DIR}
 
 exit 0
