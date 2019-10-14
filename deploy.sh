8a23ef9fc06a8ed68358f9d3206b437a7b61ca68
Walmes Zeviani <walmes@ufpr.br>
2019-10-14 17:56:12 -0300

    Updates script to deploy website.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ USER=rday
 HOST=leg.ufpr.br
 DIR=/home/rday/
 
-Rscript -e 'rmarkdown::render_site()' && rsync -avz docs/ ${USER}@${HOST}:${DIR}
+hugo && rsync -avz public/ --rsh="ssh -p$PATAXOP" ${USER}@${HOST}:${DIR}
 
 exit 0
