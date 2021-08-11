093ccfcef5fdf9a94abad55946f7e82cc685a0c6
Fernando Mayer <fernandomayer@gmail.com>
2021-08-10 22:29:02 -0300

    Arruma deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 USER=rday
 HOST=leg.ufpr.br
 DIR=/home/rday/
+read -p 'PORTA: ' PORT
 
-# hugo && 
-rsync -avz public/ --rsh="ssh -p$PATAXOP" ${USER}@${HOST}:${DIR}
+Rscript -e 'blogdown::build_site()' && rsync -avz -e "ssh -p $PORT" --delete --exclude-from 'rsync-exclude.txt' public/ ${USER}@${HOST}:${DIR}
 
 exit 0
